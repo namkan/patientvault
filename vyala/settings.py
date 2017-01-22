bbf84b5a40f241825f311d074b4eb1a9c68e21b1
bikram <bikram.bharti99@gmail.com>
2017-01-22 08:12:30 -0500

    settings

--- vyala/settings.py
+++ vyala/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,6 +86,10 @@ DATABASES = {
     }
      
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
 EMAIL_HOST_USER = 'naman.kansal@vyalatech.com'
